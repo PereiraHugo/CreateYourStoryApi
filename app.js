9337b62c17cdc18598e7b7389a09192bdb6f3e75
Hugo Pereira <PereiraHugo@users.noreply.github.com>
2018-10-24 17:15:56 +1200

    Updating package.json

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
 const querystring = require('querystring');
 const mongoose = require('mongoose')
 
-app.use(cors())
 
 var routes = require("./routes/routes.js");
-
-//var bodyParser = require("body-parser");
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,5 +29,5 @@ routes(app);
 const port = process.env.PORT || 3000; //env variable PORT if not define use 3000.
 var server = app.listen(port, function () {
     console.log("app running on port.", server.address().port);
-    //console.log(mongoose.connection.readyState);
+    console.log(mongoose.connection.readyState);
 });
